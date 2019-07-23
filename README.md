0e9730e4cdec5f8f8aeb56b7461ba01301464880
Nikoloz Kokhreidze <nkokhreidze@protonmail.com>
2019-07-23 22:51:17 +0200

    Added screenshot functionality

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
 # webbr #
 
-webbr is a simple tool that helps you identify if websites are up and running, their HTTP status codes and associated IP addresses.
+webbr is a simple tool that helps you retrieve http status codes for list of websites, take website screenshots with Chrome
+
+### How to ###
+#### Install ####
+
+`pip install -r requirements.txt`
+
+#### Run ####
+`python webbr.py`
+
+Source and destination files should be in following format `[title].xlsx`
+
+Table header must be `input_urls`. Example:
+
+| input_urls      | X           | Z  |
+| ------------- |:-------------:| -----:|
+| http://example.com    | -| - |
+| https://example2.com   |  -   |  -  |
+
+URLs must start with `http://` or `https://`
+
+### Features ###
+
+* Select Excel file with the list of URLs
+* Return HTTP status codes (https://en.wikipedia.org/wiki/List_of_HTTP_status_codes)
+* Return IP address
+* Write output to specified Excel file
+* Take website screenshots with Chrome and store in 'screenshots' directory: [1] For all websites [2] Only for errors [3] None
+* Screenshots fuinctionality is based on headless Chrome, images will be taken in the background and thus consume less resources
 
 ### Useful for ###
 
 * Vulnerability scanning - identify websites/domains that are available from a list
 * Penetration testing - identify IP addresses for domains that you can use for infrastructure testing
+* Troubleshooting website issues in batch
 * Creating domain lists
 
 ### Comments ###
 
-Tool is in development and I am not a professional Python developer.
-Any feedback is more then welcome!
+Tool is in development. Any feedback is more than welcome!
--- webbr.py
+++ webbr.py
@@ -1,12 +1,37 @@
-import requests
+import os
+import sys
 import socket
+from datetime import datetime
+import requests
 import tldextract
 import pandas as pd
-from pandas import ExcelWriter
 from pandas import ExcelFile
+from pandas import ExcelWriter
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Define time for unique screenshot names
+screenshotTime = datetime.strftime(datetime.now(), ' %Y-%m-%d %H-%M-%S')
+
+# Chrome Driver configuration for taking screenshots
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-infobars")  
+chrome_options.add_argument("--disable-extensions") 
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_driver = '.\\driver\\chromedriver.exe'
+
+# Ask for screenmshots
+takeScreens = input("Take website screenshots? (Chrome required) [1] For all websites [2] Only for errors [3] None:  ")
+
+# Ask for file
+inputFile = input("Source excel file:  ")
+outputFile = input("Destination excel file: ")
 
 # File location
-src = pd.read_excel('websites.xlsx', sheet_name='Sheet1')
+src = pd.read_excel(inputFile)
 
 # Count number of rows in excel file
 countRow = src.shape[0]
@@ -29,7 +54,6 @@ for i in src.index:
 
 # Check URLs and add them to the list
     urlList.append(domain)
-    # print(domain)
 
 # Get IP from domain
     try:
@@ -52,6 +76,19 @@ for i in src.index:
         codeList.append("error")
         status = "error"
 
+# Take screenshots based on user input
+    if takeScreens == '1':
+        driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
+        driver.get(src['input_urls'][i])
+        driver.save_screenshot('.\\screenshots\\' + domain + screenshotTime + '.png')
+        driver.close()
+
+    elif takeScreens == '2' and status != 200:
+        driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
+        driver.get(src['input_urls'][i])
+        driver.save_screenshot('.\\screenshots\\' + domain + screenshotTime + '.png')
+        driver.close()
+
     print(str(domain) + " -  " + str(status) + " - " + str(ip))
 
 # Put output in data
@@ -60,6 +97,6 @@ data = {'urls': urlList, 'status': codeList, 'ip': ipList}
 # Create a dataframe and write to new excel file
 df = pd.DataFrame(data=data, index=None)
 print("\n" + "=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#= \n" + "= RESULT:                             = \n" + "=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#= \n" + str(df))
-writer = ExcelWriter('url_status_ip.xlsx')
+writer = ExcelWriter(outputFile)
 df.to_excel(writer,'Sheet1', index=False)
 writer.save()
