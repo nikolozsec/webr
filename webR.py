52a67dbd0aa9e8fe18046ae81b96f11bdbe6b1c3
Nikoloz Kokhreidze <nkokhreidze@pm.me>
2020-09-20 23:21:50 +0200

    fix

--- webR.py
+++ webR.py
@@ -10,6 +10,12 @@ from pandas import ExcelWriter
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# Check OS and use relevant driver
+if platform.system() == 'Windows':
+    chrome_driver = '.\\driver\\chromedriver.exe'
+else:
+    chrome_driver = './driver/chromedriver'
+
 # Define time for unique screenshot names
 screenshotTime = datetime.strftime(datetime.now(), ' %Y-%m-%d %H-%M-%S')
 
@@ -23,44 +29,31 @@ chrome_options.add_argument("--window-size=1366,768")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument('--ignore-certificate-errors')
 
-# Check OS and use relevant driver
-if platform.system() == 'Windows':
-    chrome_driver = '.\\driver\\chromedriver.exe'
-else:
-    chrome_driver = './driver/chromedriver'
-
 # Ask for screenshots
-takeScreens = input("Take website screenshots? (Chrome required) [1] For all websites [2] Only for non HTTP 200 responses (for errors) [3] No screenshots: ")
+take_screens = input("Take website screenshots? (Chrome required) [1] For all websites [2] Only for non HTTP 200 responses (for errors) [3] No screenshots: ")
 
 # Ask for file
-inputFile = './domains.xlsx'
-inputColumn = 'input_urls'
-outputFile = './results.xlsx'
+input_file = './domains.xlsx'
+input_column = 'input_urls'
+output_file = './results.xlsx'
 
 # File location
-src = pd.read_excel(inputFile)
-column = src[inputColumn]
+src = pd.read_excel(input_file)
+column = src[input_column]
 
 # Count number of rows in excel file
 countRow = src.shape[0]
 
 # Create empty list
-urlList = []
-codeList = []
-ipList = []
+url_list = []
+code_list = []
+ip_list = []
 
 # Print a message
-
-
 
 print("\n" + "=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#= \n" + "=     webbr by Nikoloz Kokhreidze     = \n" + "=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#= \n")
 print("CHECKING {} ITEMS: \n".format(countRow))
 
-
-# if 'browserVersion' in driver.capabilities:
-#     print(driver.capabilities['browserVersion'])
-# else:
-#     print(driver.capabilities['version'])
 
 # Go throught the File
 for i in src.index:
@@ -71,37 +64,37 @@ for i in src.index:
     httpUrl = 'http://' + domain
 
 # Add URLs to the list
-    urlList.append(httpUrl)
+    url_list.append(httpUrl)
 
 # Get IP from domain
     try:
         ip = socket.gethostbyname(domain)
-        ipList.append(ip)
+        ip_list.append(ip)
 
 # If error, write error in excel
     except socket.error as error:
-        ipList.append("error")
+        ip_list.append("error")
         ip = "error"
 
 # Try to send request
     try:
         r = requests.get(httpUrl, stream=True)
         status = r.status_code
-        codeList.append(status)
+        code_list.append(status)
 
 # If error, write error in excel
     except requests.ConnectionError as error: 
-        codeList.append("error")
+        code_list.append("error")
         status = "error"
 
 # Take screenshots based on user input
-    if takeScreens == '1':
+    if take_screens == '1':
         driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
         driver.get(column[i])
         driver.save_screenshot('.\\screenshots\\' + domain + screenshotTime + '.png')
         driver.close()
 
-    elif takeScreens == '2' and status != 200:        
+    elif take_screens == '2' and status != 200:        
         driver = webdriver.Chrome(options=chrome_options, executable_path=chrome_driver)
         driver.get(column[i])
         driver.save_screenshot('.\\screenshots\\' + domain + screenshotTime + '.png')
@@ -110,11 +103,11 @@ for i in src.index:
     print(str(domain) + " -  " + str(status) + " - " + str(ip))
     
 # Put output in data
-data = {'URL': urlList, 'Status': codeList, 'IP': ipList}
+data = {'URL': url_list, 'STATUS': code_list, 'IP': ip_list}
 
 # Create a dataframe and write to new excel file
 df = pd.DataFrame(data=data, index=None)
 print("\n" + "=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#= \n" + "= RESULT:                             = \n" + "=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#= \n" + str(df))
-writer = ExcelWriter(outputFile)
+writer = ExcelWriter(output_file)
 df.to_excel(writer,'webbr', index=False)
 writer.save()
